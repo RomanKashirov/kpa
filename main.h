a667fd9c40b9e02980e09fbe8d069a98df8cdcd1
roman <kashirov@mail.ru>
2020-02-10 18:18:23 +0700

    добавил заготовки файлов ethernet

--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 Подключение 1986ВЕ91Т к Ethernet с помощью 5600ВГ1У. Интерфейс параллельный.
 10.02.2020
 *************************************************************************/
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include "config.h"
 //#include "5600VG1U_serial.h"
@@ -10,3 +12,5 @@
 
 #define SET_LED5() MDR_PORTD->RXTX = (1<<14)|(MDR_PORTD->RXTX & 0xFFE0)
 #define RESET_LED5() MDR_PORTD->RXTX &= ~((1<<14)|0x001F);
+
+#endif /* _MAIN_H_ */
