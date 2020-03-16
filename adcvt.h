e6cd337ee2b197c096d33cf0194ea831dffeac35
roman <kashirov@mail.ru>
2020-03-16 13:28:19 +0700

    побавлено подлуючение main.h так как там задефайнен NULL временно изменена размерность буфера с результатами

--- adcvt.h
+++ adcvt.h
@@ -5,7 +5,9 @@
 #ifndef _ADCVT_H_
 #define _ADCVT_H_
 
-#define ADCVT_BUFFER_SIZE 32
+#include "main.h"
+
+#define ADCVT_BUFFER_SIZE 4
 
 typedef enum  {Ch1 = 0, Ch2 = 1, Ch3 = 2, Ch4 = 3, Ch5 = 4, Ch6 = 5} Channel_Adc_VT;
 
@@ -26,7 +28,9 @@ typedef struct
 	unsigned int tail;			//хвост
 } measure_buf;
 
+measure_data* Get_measure_data(void); // Если данные есть, то возвращает ссылку на структуру с данными. Есл нет то возвращает NULL
 
+unsigned int Check_measure_data(void); // Возвращает количество данных
 
 void Get_adcvt_data(unsigned int channels);
 
