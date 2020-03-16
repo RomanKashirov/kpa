d1fef1f7fd2dda9c63c5abc58c672f63f5e42c7e
roman <kashirov@mail.ru>
2020-03-16 13:31:03 +0700

    Переименована ф-ция Get_channel_data в Put_channel_data Изменена реализация кольцевого буфера. вместо %size применяется &(size-1) Добавлены функции Check_measure_data и Get_adcvt_data

--- adcvt.c
+++ adcvt.c
@@ -18,15 +18,15 @@ unsigned int Get_angle(Channel_Adc_VT ch)
 }
 
 // Считывание данных из заданного канала АЦПВТ
-void Get_channel_data(Channel_Adc_VT ch)
+void Put_channel_data(Channel_Adc_VT ch)
 {
 	Adcvt_data.data_buf[Adcvt_data.head].angle = Get_angle(ch);
 	Adcvt_data.data_buf[Adcvt_data.head].Ch = ch;
 	Adcvt_data.data_buf[Adcvt_data.head].time = current_time;
-	Adcvt_data.head = (Adcvt_data.head + 1) % ADCVT_BUFFER_SIZE;
+	Adcvt_data.head = (Adcvt_data.head + 1) & (ADCVT_BUFFER_SIZE-1);
 	if(Adcvt_data.head == Adcvt_data.tail)
 	{
-		Adcvt_data.tail = (Adcvt_data.tail + 1) % ADCVT_BUFFER_SIZE;
+		Adcvt_data.tail = (Adcvt_data.tail + 1) & (ADCVT_BUFFER_SIZE-1);
 	}
 }
 
@@ -36,32 +36,32 @@ void Get_adcvt_data(unsigned int channels)
 {
 	if(channels & (1<<Ch1))
 	{
-		Get_channel_data(Ch1);
+		Put_channel_data(Ch1);
 	}
 	
 	if(channels & (1<<Ch2))
 	{
-		Get_channel_data(Ch2);
+		Put_channel_data(Ch2);
 	}
 	
 	if(channels & (1<<Ch3))
 	{
-		Get_channel_data(Ch3);
+		Put_channel_data(Ch3);
 	}
 	
 	if(channels & (1<<Ch4))
 	{
-		Get_channel_data(Ch4);
+		Put_channel_data(Ch4);
 	}
 		
 	if(channels & (1<<Ch5))
 	{
-		Get_channel_data(Ch5);
+		Put_channel_data(Ch5);
 	}
 	
 	if(channels & (1<<Ch6))
 	{
-		Get_channel_data(Ch6);
+		Put_channel_data(Ch6);
 	}
 }
 
@@ -70,6 +70,32 @@ void Initialize_adc_vt(void)
 {
 	Adcvt_data.head = 0;
 	Adcvt_data.tail = 0;
+}
+
+
+measure_data* Get_measure_data(void) // Если данные есть, то возвращает ссылку на структуру с данными. Есл нет то возвращает NULL
+{
+	if(Adcvt_data.head == Adcvt_data.tail)
+	{
+		return NULL;
+	}
+	else
+	{
+		Adcvt_data.tail = (Adcvt_data.tail + 1) & (ADCVT_BUFFER_SIZE - 1);
+		return Adcvt_data.data_buf + Adcvt_data.head;
+	}
+}
+
+unsigned int Check_measure_data(void) // Возвращает количество данных
+{
+	if(Adcvt_data.head >= Adcvt_data.tail)
+	{
+		return Adcvt_data.head - Adcvt_data.tail;
+	}
+	else
+	{
+		return Adcvt_data.head + ADCVT_BUFFER_SIZE - Adcvt_data.tail;
+	}
 }
 
 
